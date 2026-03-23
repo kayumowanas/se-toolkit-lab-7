330cba9d9f1adc97feb97ba4a1277f41c92d8307
kayumowanas <kayumowanas@gmail.com>
2026-03-23 22:43:19 +0300

    Complete setup and task 1

---
+++ bot/handlers/commands/__init__.py
@@ -0,0 +1,19 @@
+from .basic import (
+    handle_health,
+    handle_help,
+    handle_labs,
+    handle_plain_text,
+    handle_scores,
+    handle_start,
+    handle_unknown,
+)
+
+__all__ = [
+    "handle_health",
+    "handle_help",
+    "handle_labs",
+    "handle_plain_text",
+    "handle_scores",
+    "handle_start",
+    "handle_unknown",
+]
